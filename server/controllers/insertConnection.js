a98a387cb25c2b65d2d496613300856b85db043f
Alan Zheng <aszheng12@gmail.com>
2017-06-16 00:15:36 -0700

    Slice daily_all_matches on response submit

--- server/controllers/insertConnection.js
+++ server/controllers/insertConnection.js
@@ -1,8 +1,7 @@
 const models = require('../../db/models');
+const knex = require('knex')(require('../../knexfile'));
 
 module.exports.insertConnection = (req, res) => {
-  console.log('')
-
   models.Connection.forge({
     users_a_id: req.body.users_a_id,
     users_b_id: req.body.users_b_id,
@@ -13,4 +12,14 @@ module.exports.insertConnection = (req, res) => {
     .then(result => {
       res.send(result.attributes);
     })
+}
+
+module.exports.sliceMatch = (req, res) => {
+  // knex
+  // .raw("update users set daily_all_matches = ? where id = ?;", [req.body.daily_all_matches, req.body.id])
+  // .then(result => {
+  //   console.log('DONE')
+  //   res.status(202).send(result);
+  // });
+  res.send('TEST')
 }
